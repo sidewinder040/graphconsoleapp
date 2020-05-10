e659187ed90934de00c90e1bcabd36efe7e52c93
sidewinder040 <macevic040@hotmail.co.uk>
2020-05-10 18:45:50 +0100

    Project completed

--- Program.cs
+++ Program.cs
@@ -25,17 +25,24 @@ namespace graphconsoleapp
             var client = GetAuthenticatedGraphClient(config);
 
             // var graphRequest = client.Users.Request();
-            var graphRequest = client.Users
-                    .Request()
-                    .Select(u => new { u.DisplayName, u.Mail })
-                    .Top(15)
-                    //.OrderBy("DisplayName desc");
-                    .Filter("startsWith(surname,'A') or startsWith(surname,'B') or startsWith(surname,'Z')");
+            // var graphRequest = client.Users
+            //         .Request()
+            //         .Select(u => new { u.DisplayName, u.Mail })
+            //         .Top(15)
+            //         //.OrderBy("DisplayName desc");
+            //         .Filter("startsWith(surname,'A') or startsWith(surname,'B') or startsWith(surname,'Z')");
+            var graphRequest = client.Groups.Request().Top(5).Expand("members");
 
             var results = graphRequest.GetAsync().Result;
-            foreach(var user in results)
+            // foreach(var user in results)
+            foreach(var group in results)
             {
-                Console.WriteLine(user.Id + ": " + user.DisplayName + " <" + user.Mail + ">");
+                // Console.WriteLine(user.Id + ": " + user.DisplayName + " <" + user.Mail + ">");
+                 Console.WriteLine(group.Id + ": " + group.DisplayName);
+                foreach(var member in group.Members)
+                {
+                    Console.WriteLine("  " + member.Id + ": " + ((Microsoft.Graph.User)member).DisplayName);
+                }
             }
 
             Console.WriteLine("\nGraph Request:");
